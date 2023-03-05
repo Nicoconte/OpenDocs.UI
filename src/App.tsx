8c7c95591c775f3f912f0120436911ba809d7cb2
NicoConte <nicoconte1999@gmail.com>
2023-03-05 01:45:29 -0300

    (feat): Init commit. Create project structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-slate-300">
+      <h1 className='text-3xl text-red-600'>Holaaa</h1>
+    </div>
+  )
+}
+
+export default App
