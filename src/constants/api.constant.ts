5b07ff9d900a32fe818cd508ce9d805a32b05e03
Nicolas Conte <nicoconte1999@gmail.com>
2023-03-09 00:20:42 -0300

    (Feat): Add env variables

---
+++ src/constants/api.constant.ts
@@ -0,0 +1,6 @@
+export const apiUrls = {
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    applicationEndpoint: import.meta.env.VITE_ENDPOINT_APPLICATION,
+    groupEndpoint: import.meta.env.VITE_ENDPOINT_GROUPS,
+    environmentEndpoint: import.meta.env.VITE_ENDPOINT_ENVIRONMENT   
+}
